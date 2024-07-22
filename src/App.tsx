78d2a6aa7dfbc7d1e3e6968da3236461357a2e43
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-07-22 22:35:12 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button/Button"
+import Input from "./components/Input/Input"
+
+function App() {
+
+
+  return (
+    <>
+      <Button >Button</Button>
+      <Button appearence="big">Кнопка</Button>
+      <Input isValid={true} placeholder="Email"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import styles from "./Input.module.css";
+import cn from "classnames";
+import { forwardRef } from "react";
+import { InputProps } from "./Input.props";
+
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { className, isValid = true, ...props },
+  ref,
+) {
+  return (
+    <input
+      ref={ref}
+      className={cn(className, cn(styles["input"], {
+        [styles["invalid"]]: !isValid
+      }))} {...props}
+    />
+  );
+});
+
+export default Input;
