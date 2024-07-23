183711c41efa965646143395e8b3513f1103fe1d
Юлия Шкатула <yshkatula@bostonsd.ru>
2024-07-23 16:31:11 +0300

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from "react-router-dom"
 import Button from "./components/Button/Button"
 import Input from "./components/Input/Input"
+import { Menu } from "./pages/Menu/Menu"
+import { Cart } from "./pages/Cart/Cart"
+import { Error } from "./pages/Error/Error"
 
 function App() {
 
@@ -9,6 +13,15 @@ function App() {
       <Button >Button</Button>
       <Button appearence="big">Кнопка</Button>
       <Input isValid={true} placeholder="Email"/>
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes >
+        <Route path="/" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Error />}
+      </Routes>
     </>
   )
 }
