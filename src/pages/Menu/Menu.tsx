3451a3e043f96fcc1a0894ee1144d97670536175
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-08-03 12:42:12 +0300

    ProductCard

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,5 @@
 import Heading from "../../components/Heading/Heading";
+import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
 import styles from './Menu.module.css'
 
@@ -7,5 +8,16 @@ export function Menu() {
         <div className={styles['head']}>
            <Heading>Menu</Heading>
             <Search placeholder="Введите блюдо или состав"/>     
-        </div></>
+        </div>
+        <div>
+              <ProductCard
+              id={1}
+              title="Наслаждение"
+              description="Салями, руккола, помидоры, оливки"
+              rating={4.5}
+              price={300}
+              image='/product-demo.png'
+              />  
+        </div>    
+    </>
 }
