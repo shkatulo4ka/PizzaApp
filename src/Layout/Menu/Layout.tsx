c411894840f6d52a5692abccccb8a3b9bda9dab4
Shkatylo4ka <Shkatylo4ka@gmail.com>
2024-09-03 15:46:28 +0300

    user profile

--- src/Layout/Menu/Layout.tsx
+++ src/Layout/Menu/Layout.tsx
@@ -2,11 +2,22 @@ import { NavLink, Outlet, useNavigate } from "react-router-dom";
 import styles from './Layout.module.css';
 import Button from "../../components/Button/Button";
 import cn from 'classnames';
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import { getProfile, userActions } from "../../store/user.slice";
+import { useEffect } from "react";
 
 export function Layout() {
     const navigate = useNavigate();
+    const dispatch = useDispatch<AppDispatch>();
+    const profile = useSelector((s: RootState) => s.user.profile)
+
+    useEffect(() => {
+        dispatch(getProfile())
+    }, [dispatch])
+
     const logout = () => {
-        localStorage.removeItem('jwt');
+        dispatch(userActions.logout())
         navigate('/auth/login');
     }
 
@@ -14,8 +25,8 @@ export function Layout() {
         <div className={styles['sidebar']}>
             <div className={styles['user']}>
                 <img className={styles['avatar']} src="/avatar.svg" alt="Аватар" />
-                <div className={styles['name']}>Антон Ларичев</div>
-                <div className={styles['email']}>example@mail.ru</div>
+                <div className={styles['name']}>{profile?.name}</div>
+                <div className={styles['email']}>{profile?.email}</div>
             </div>
             <div className={styles['menu']}>
                 <NavLink to="/" className={({ isActive }) => cn(styles['link'], 
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,13 +3,10 @@ import Heading from "../../components/Heading/Heading";
 import Input from "../../components/Input/Input";
 import { Link, useNavigate } from "react-router-dom";
 import styles from './Login.module.css';
-import { FormEvent, useState } from "react";
-import { PREFIX } from "../../helpers/API";
-import axios, { AxiosError } from "axios";
-import { LoginResponse } from "../../Interfaces/auth.interface";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../store/store";
-import { userActions } from "../../store/user.slice";
+import { FormEvent, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../store/store";
+import { login, userActions } from "../../store/user.slice";
 
 export type LoginForm = {
     email: {
@@ -21,38 +18,34 @@ export type LoginForm = {
 }
 
 export function Login() {
-    const [error, setError] = useState<string | undefined>();
     const navigate = useNavigate();
     const dispatch  = useDispatch<AppDispatch>();
+    const { jwt, loginErrorMessage } = useSelector((s: RootState) => s.user);
+
+
+    useEffect(() => {
+        if (jwt) {
+            navigate('/')
+        }
+    }, [jwt, navigate])
     
     const submit = async (e: FormEvent) => {
         e.preventDefault();
-        setError(undefined);
+        dispatch(userActions.clearLoginError());
         const target = e.target as typeof e.target & LoginForm;
         const {email, password} = target;
         await sendLogin(email.value, password.value);
     };
 
     const sendLogin = async (email: string, password: string) => {
-        try {
-            const {data} = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
-                email,
-                password
-            }) ;
-            localStorage.setItem('jwt', data.access_token);
-            dispatch(userActions.addJwt(data.access_token));
-            navigate('/');
-        } catch(e) {
-            if (e instanceof AxiosError) 
-            setError(e.response?.data.message)
-        }
-         
+        dispatch(login({email, password}))
+        
     }
 
 
     return <div className={styles['login']}>
     <Heading>Вход</Heading>
-    {error && <div className={styles['error']}>{error}</div>}
+    {loginErrorMessage && <div className={styles['error']}>{loginErrorMessage}</div>}
     <form className={styles['form']} onSubmit={submit}>
         <div className={styles['field']}>
             <label htmlFor="email">Ваш email</label>
